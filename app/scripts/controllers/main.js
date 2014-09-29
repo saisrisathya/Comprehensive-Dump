4cd08a79b713e4b9524e2721ed538dd821b99da9
saisrisathya <saisrisathya@gmail.com>
2014-09-29 10:21:57 +0530

    Added dump

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the comprehensiveApp
  */
 angular.module('comprehensiveApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
+  .controller('MainCtrl', function ($scope, $http) {
+    /*$scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
-    ];
+    ];*/
+
+    $http.get('data/dump.json').
+    success(function(data, status, headers, config) {
+      $scope.questions = data;
+    }).
+    error(function(data, status, headers, config) {
+      // log error
+    });
   });
